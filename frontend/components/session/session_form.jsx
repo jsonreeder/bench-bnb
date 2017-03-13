b9a2a7af3f7ef21b245e16029818789772e63598
Jason Reeder <info@jasonreeder.com>
2017-03-13 10:42:05 -0700

    Add basic form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -10,6 +10,28 @@ class SessionForm extends React.Component {
   }
 
   // TODO: Add methods
+  // TODO: Add styling
+  render() {
+    return (
+      <div>
+        <form>
+          <h2>Welcome to BenchBnb</h2>
+          <label>
+            Username:
+            <input type='text'
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input type='password' />
+          </label>
+          <br />
+          <input type='submit' value='submit' />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SessionForm;
